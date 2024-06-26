c4fc9e1e1b4dcb192df23a72f76b4efc74242a80
100ask <100ask@100ask.com>
2024-06-26 19:41:42 +0800

    初次尝试

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define A 3
+#define B(A) ((A+1)*a)
+
+int main()
+{
+    printf("Hello world!\n");
+    int a = 3;
+    int X;
+    X=3*(A+B(7));
+    printf("%d",X);
+
+
+    return 0;
+}
